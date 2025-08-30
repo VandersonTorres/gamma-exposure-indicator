697c62b673b293b458552c68f6fc72a4a5e7220f
Vanderson Torres <vanderson.torres06@gmail.com>
2025-08-29 22:18:37 -0300

    Final Implementation

--- src/settings.py
+++ src/settings.py
@@ -2,6 +2,9 @@ import logging
 import os
 
 
+log_format = "[%(asctime)s] [%(name)s] - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format)
+
 DOWNLOADS_BASE_DIR = os.path.join(os.getcwd(), "data")
 
 RAW_DIR = os.path.join(DOWNLOADS_BASE_DIR, "raw")
@@ -13,6 +16,3 @@ os.makedirs(RAW_DIR, exist_ok=True)
 os.makedirs(PROCESSED_DIR, exist_ok=True)
 os.makedirs(REPORTS_DIR, exist_ok=True)
 os.makedirs(TEMP_DIR, exist_ok=True)
-
-LOG_FORMAT = "[%(asctime)s] [%(name)s] - %(levelname)s - %(message)s"
-logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
